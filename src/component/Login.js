7f906f6881cbf511f6f8b69c0bd2364ac2f9b99b
Shujon Mollah <mollah@gmail.com>
2018-01-02 16:50:27 -0500

    FIX missing props by rendering the smart NOT dumb component

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Header from './Header'
-import LoginForm from './LoginForm'
+import LoginForm from '../container/LoginForm'
 import Footer from './Footer'
 
 export default () => (
