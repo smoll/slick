1bda8c46f00c5519592fe93ed6668883a65b4f4b
Shujon Mollah <mollah@gmail.com>
2018-01-02 17:15:55 -0500

    Extract MainPanel HoC and clean up

--- src/component/LoginForm.js
+++ src/component/LoginForm.js
@@ -2,57 +2,66 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {Grid, Row, Col} from 'react-flexbox-grid'
 
+import MainPanel from './MainPanel'
+
 class LoginForm extends Component {
   render() {
     const {login} = this.props
     return (
-      <Grid fluid>
-        <Row style={{paddingTop: '2em'}}>
-          <Col xs={1} sm={2} lg={4} />
-          <Col xs={12} sm={8} lg={4} >
-            <div className='pt-card pt-elevation-3'>
-              <h5>Please login to continue</h5>
+      <MainPanel>
+        <div className='pt-card pt-elevation-3'>
+          <h5 style={styles.title}>Please login to continue</h5>
 
-              <div className='pt-form-group' style={{paddingTop: '1em'}}>
-                <label className='pt-label' htmlFor='email'>
-                  Email
-                </label>
-                <div className='pt-form-content'>
-                  <input id='email' className='pt-input' style={{width: '100%'}} placeholder='test@test.com' type='email' dir='auto' />
-                  <div className='pt-form-helper-text'>(Hint: test@test.com)</div>
-                </div>
-              </div>
-
-              <div className='pt-form-group'>
-                <label className='pt-label' htmlFor='password'>
-                  Password
-                </label>
-                <div className='pt-form-content'>
-                  <input id='password' className='pt-input' style={{width: '100%'}} type='password' dir='auto' />
-                  <div className='pt-form-helper-text'>(Hint: test1234)</div>
-                </div>
-              </div>
-
-              <Row end='xs'>
-                <Col style={{paddingTop: '1em', paddingRight: '0.5em'}}>
-                  <button type='button' className='pt-button pt-intent-primary' onClick={login}>
-                    Login
-                    <span className='pt-icon-standard pt-icon-arrow-right pt-align-right'></span>
-                  </button>
-                </Col>
-              </Row>
-
+          <div className='pt-form-group'>
+            <label className='pt-label' htmlFor='email'>
+              Email
+            </label>
+            <div className='pt-form-content'>
+              <input id='email' className='pt-input' style={styles.input} placeholder='test@test.com' type='email' dir='auto' />
+              <div className='pt-form-helper-text'>(Hint: test@test.com)</div>
             </div>
-          </Col>
-          <Col xs={1} sm={2} lg={4} />
-        </Row>
-      </Grid>
+          </div>
+
+          <div className='pt-form-group'>
+            <label className='pt-label' htmlFor='password'>
+              Password
+            </label>
+            <div className='pt-form-content'>
+              <input id='password' className='pt-input' style={styles.input} type='password' dir='auto' />
+              <div className='pt-form-helper-text'>(Hint: test1234)</div>
+            </div>
+          </div>
+
+          <div style={styles.buttons}>
+            <button type='button' className='pt-button pt-intent-primary' onClick={login}>
+              Login
+              <span className='pt-icon-standard pt-icon-arrow-right pt-align-right'></span>
+            </button>
+          </div>
+
+        </div>
+      </MainPanel>
     )
   }
 }
 
 LoginForm.propTypes = {
   login: PropTypes.func.isRequired,
+}
+
+const styles = {
+  title: {
+    paddingBottom: '1em',
+  },
+  input: {
+    width: '100%',
+  },
+  buttons: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    paddingTop: '1em',
+    paddingRight: '0.5em',
+  },
 }
 
 export default LoginForm
